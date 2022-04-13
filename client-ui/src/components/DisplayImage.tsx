de77f17d640ade0d7d70c291818a3ad58f530d5b
Justin Thoreson <thoresonjd@gmail.com>
2022-04-13 10:26:08 -0700

    Image containment

--- client-ui/src/components/DisplayImage.tsx
+++ client-ui/src/components/DisplayImage.tsx
@@ -7,7 +7,7 @@ interface Props {
 const DisplayImage: React.FC<Props> = ({image}) => {
   return (
     <div className='display-image center'>
-      <img src={image} alt="image" width="200px" />
+      <img src={image} alt='image' className='image'/>
     </div>
   );
 }
