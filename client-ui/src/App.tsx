ecdc051434973204ff6d3367f91e71172096e733
Justin Thoreson <thoresonjd@gmail.com>
2022-04-12 22:08:36 -0700

    App theme set to dark

--- client-ui/src/App.tsx
+++ client-ui/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import ImageProcessorToolbar from './components/ImageProcessorToolbar';
+import darkTheme from './theme/Theme';
 
 const App: React.FC = () => {
   return (
-    <>
-      Image Processor
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <ImageProcessorToolbar />
+    </ThemeProvider>
   );
 }
 
