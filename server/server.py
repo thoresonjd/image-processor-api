3518dc457aa0645fc56fc67706ff76a1878aea22
Justin Thoreson <thoresonjd@gmail.com>
2022-04-13 22:24:18 -0700

    RESTful server functional with GET request

--- server/server.py
+++ server/server.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_restful.utils import cors
 
 app = Flask(__name__)
 api = Api(app)
+api.decorators=[cors.crossdomain(origin='*')]
+
+class ImageProcessor(Resource):
+  def get(self):
+    return {"json": "data"}
+
+api.add_resource(ImageProcessor, '/')
 
 def main() -> None:
   app.run(port=6969, debug=True)
