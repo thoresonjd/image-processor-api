9362c04ba969f9d3336e2eb9e5e6914cb78322c6
Justin Thoreson <thoresonjd@gmail.com>
2022-04-17 20:00:36 -0700

    Successfully verifies MIME type of input file w/ ImageVerifier implemented

--- api/ImageVerifier.py
+++ api/ImageVerifier.py
@@ -1,5 +1,12 @@
+from json import load
+
 class ImageVerifier:
 
-  @staticmethod
-  def verify(image: str):
-    pass
+  def __init__(self) -> None:
+    self.__mimeTypes: dict = load(open("MIMETypes.json"))
+
+  def isVerifiedImage(self, image: str) -> bool:
+    for mimeType in self.__mimeTypes.keys():
+      if image[0:len(mimeType)] == mimeType:
+        return True
+    return False
--- api/TransfomationPurifier.py
+++
@@ -1,5 +0,0 @@
-class TransformationPurifier:
-
-  @staticmethod
-  def purify(transformations: dict) -> dict:
-    pass
