a234f0459f0d0b347f1a281d3fcc9ea46dc49743
Justin Thoreson <thoresonjd@gmail.com>
2022-04-17 20:01:38 -0700

    Image class can save JPEGs and PNGs

--- api/Image.py
+++ api/Image.py
@@ -1,17 +1,27 @@
+from distutils import extension
 from PIL import Image as PillowImage
 from io import BytesIO
 from Transformation import Transformation
 from TransformationBuilder import TransformationBuilder
+from json import load
 import base64
 
 class Image:
   """Allows Base64 image manipulation"""
 
-  def __init__(self, image) -> None:
+  def __init__(self, image: str) -> None:
     self.__image: PillowImage = self.__decode(image)
+    self.__extension: str = self.__getExtension(image)
     self.__tb: TransformationBuilder = TransformationBuilder()
 
-  def __decode(self, image) -> PillowImage:
+  def __getExtension(self, image: str) -> str:
+    mimeTypes: dict = load(open("MIMETypes.json"))
+    for mimeType in mimeTypes.keys():
+      if image[0:len(mimeType)] == mimeType:
+        return mimeTypes[mimeType]
+    return "JPEG"
+
+  def __decode(self, image: str) -> PillowImage:
     """Converts a Base64 string representation of an image to a PIL Image
     
     :param image: Base64 string representation of an image
@@ -30,7 +40,7 @@ class Image:
 
     # TODO: Return as given input format, not just JPEG
     buffered: BytesIO = BytesIO()
-    self.__image.save(buffered, format="JPEG")
+    self.__image.save(buffered, format=self.__extension)
     encodedImage: bytes = base64.b64encode(buffered.getvalue())
     return ''.join(map(chr, encodedImage)) 
 
