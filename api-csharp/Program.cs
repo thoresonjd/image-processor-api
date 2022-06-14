27c972469bd8271686f739d4eaa62b15fafe9bfb
Justin Thoreson <thoresonjd@gmail.com>
2022-06-14 10:05:36 -0700

    Hello, world!

--- api-csharp/Program.cs
+++ api-csharp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ImageProcessor {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
