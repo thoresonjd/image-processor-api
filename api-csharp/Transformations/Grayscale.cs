6d478610e2e5514970b207d3aed36b5c629e997d
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:28 -0700

    Documentation for Grayscale command

--- api-csharp/Transformations/Grayscale.cs
+++ api-csharp/Transformations/Grayscale.cs
@@ -3,11 +3,19 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class Grayscale : ITransformation {
-
-    public Grayscale() {}
-
-    public void  transform(ref SharpImage image) {
+/// <summary>
+/// Grayscale<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be converted to grayscale
+/// </summary>
+class Grayscale : ITransformation 
+{
+    /// <summary>
+    /// Converts an image to grayscale
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         image.Mutate(x => x.Grayscale());
     }
 }
