ad97f30f34c8ac014784c1c70ad66d8e1678fd5e
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:00 -0700

    Documentation for ImageResponse

--- api-csharp/Models/ImageResponse.cs
+++ api-csharp/Models/ImageResponse.cs
@@ -1,6 +1,10 @@
 namespace ImageProcessor.Models;
 
-public class ImageResponse {
-    
+/// <summary>
+/// ImageResponse
+/// A model respresenting the JSON structure of an image response
+/// </summary>
+public class ImageResponse 
+{    
     public string image { get; set; } = string.Empty;
 }
