93730f8c6c54b1e5bf21629c6be39e3f00c79490
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:56 -0700

    Documentation for RotateLeft command

--- api-csharp/Transformations/RotateLeft.cs
+++ api-csharp/Transformations/RotateLeft.cs
@@ -3,11 +3,19 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class RotateLeft : ITransformation {
-
-    public RotateLeft() {}
-
-    public void transform(ref SharpImage image) {
+/// <summary>
+/// RotateLeft<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be rotated 90 degrees counter-clockwise
+/// </summary>
+class RotateLeft : ITransformation 
+{
+    /// <summary>
+    /// Rotates an image 90 degrees counter-clockwise
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         image.Mutate(x => x.Rotate(RotateMode.Rotate270));
     }
 }
