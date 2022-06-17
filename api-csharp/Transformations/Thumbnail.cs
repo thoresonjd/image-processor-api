71294bdb7cc6521e294e4089d90d1699fc750021
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:45:18 -0700

    Documentation for Thumbnail command

--- api-csharp/Transformations/Thumbnail.cs
+++ api-csharp/Transformations/Thumbnail.cs
@@ -3,24 +3,23 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class Thumbnail : ITransformation {
-
+/// <summary>
+/// Thumbnail<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be converted to a thumbnail
+/// </summary>
+class Thumbnail : ITransformation 
+{
     private const int MAX_SIZE = 100;
 
-    public Thumbnail() {}
-
-    public void transform(ref SharpImage image) {
-        int width = MAX_SIZE;
-        int height = MAX_SIZE;
-
-        if (image.Width > image.Height) {
-            int factor = image.Width/MAX_SIZE;
-            height = image.Height/factor;
-        } else if (image.Width < image.Height) {
-            int factor = image.Height/MAX_SIZE;
-            width = image.Width/factor;
-        }
-
+    /// <summary>
+    /// Creates a thumbnail of an image
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
+        int width = image.Width < image.Height ? image.Width/(image.Height/MAX_SIZE) : MAX_SIZE;
+        int height = image.Width > image.Height ? image.Height/(image.Width/MAX_SIZE) : MAX_SIZE;
         image.Mutate(x => x.Resize(width, height));
     }
 }
