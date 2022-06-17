2c05c75bdbef111cb41e7403d0cdf783b099f2fe
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:45:11 -0700

    Documentation for Saturate command

--- api-csharp/Transformations/Saturate.cs
+++ api-csharp/Transformations/Saturate.cs
@@ -3,15 +3,31 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class Saturate : ITransformation {
-
+/// <summary>
+/// Saturate<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be saturated or desaturated
+/// </summary>
+class Saturate : ITransformation 
+{
     private float saturation;
 
-    public Saturate(float saturation) {
+    /// <summary>
+    /// Constructor of the Saturate command<br />
+    /// Sets the saturation value for the Saturate command
+    /// </summary>
+    /// <param name="saturation"></param>
+    public Saturate(float saturation) 
+    {
         this.saturation = saturation;
     }
 
-    public void transform(ref SharpImage image) {
+    /// <summary>
+    /// Saturates an image
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         if (saturation < 0)
             image.Mutate(x => x.Invert());
         image.Mutate(x => x.Saturate(saturation < 0 ? -saturation : saturation));
