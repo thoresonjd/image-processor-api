35d16cb5d6762e0bfaf04d2f0958f2cb94290a6c
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:20 -0700

    Documentation for FlipVertical command

--- api-csharp/Transformations/FlipVertical.cs
+++ api-csharp/Transformations/FlipVertical.cs
@@ -3,11 +3,19 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class FlipVertical : ITransformation {
-
-    public FlipVertical() {}
-
-    public void transform(ref SharpImage image) {
+/// <summary>
+/// FlipVertical<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be flipped vertically
+/// </summary>
+class FlipVertical : ITransformation 
+{
+    /// <summary>
+    /// Performs a vertical flip on an image
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image)
+    {
         image.Mutate(x => x.Flip(FlipMode.Vertical));
     }
 }
