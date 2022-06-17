f824eba0af5bf54fbfd21ead93099a9c34cf5286
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:36 -0700

    Documentation for GrayscalePercentage command

--- api-csharp/Transformations/GrayscalePercentage.cs
+++ api-csharp/Transformations/GrayscalePercentage.cs
@@ -3,15 +3,31 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class GrayscalePercentage : ITransformation {
-
+/// <summary>
+/// GrayscalePercentage<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be converted to a specified grayscale
+/// </summary>
+class GrayscalePercentage : ITransformation 
+{
     private float percentage;
 
-    public GrayscalePercentage(float percentage) {
+    /// <summary>
+    /// Constructor of the GrayscalePercentage command<br />
+    /// Sets the percentage value for grayscale
+    /// </summary>
+    /// <param name="percentage"></param>
+    public GrayscalePercentage(float percentage) 
+    {
         this.percentage = percentage;
     }
 
-    public void transform(ref SharpImage image) {
+    /// <summary>
+    /// Converts an image to a specified grayscale
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         image.Mutate(x => x.Grayscale(percentage));
     }
 }
