b4c37cbb76be68616bcf52d6e572122c93fcdeab
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:49 -0700

    Documentation for Rotate command

--- api-csharp/Transformations/Rotate.cs
+++ api-csharp/Transformations/Rotate.cs
@@ -3,15 +3,31 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class Rotate : ITransformation {
-
+/// <summary>
+/// Rotate<br />
+/// Implements the ITransformation interface<br />
+/// Allows for an image to be rotated by specified degrees
+/// </summary>
+class Rotate : ITransformation 
+{
     private float degrees;
 
-    public Rotate(float degrees) {
+    /// <summary>
+    /// Constructor of the Rotate command<br />
+    /// Sets the degrees for rotation
+    /// </summary>
+    /// <param name="degrees"></param>
+    public Rotate(float degrees) 
+    {
         this.degrees = degrees;
     }
 
-    public void transform(ref SharpImage image) {
+    /// <summary>
+    /// Performs a rotation of specified degrees on an image
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         image.Mutate(x => x.Rotate(degrees));
     }
 }
