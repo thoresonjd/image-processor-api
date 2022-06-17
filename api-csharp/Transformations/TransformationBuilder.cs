08711c8962df05d9acb9a97973477f67bd14ef8d
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:45:25 -0700

    Documentation for TransformationBuilder

--- api-csharp/Transformations/TransformationBuilder.cs
+++ api-csharp/Transformations/TransformationBuilder.cs
@@ -1,22 +1,42 @@
 namespace ImageProcessor.Transformations;
 
-class TransformationBuilder {
-
+/// <summary>
+/// TransformationBuilder<br />
+/// Builds a list of ITransformation commands
+/// </summary>
+class TransformationBuilder 
+{
     private List<ITransformation> transformations;
 
-    public TransformationBuilder() {
+    /// <summary>
+    /// Constructor of the TransformationBuilder class<br />
+    /// Initializes an empty list of ITransformation commands
+    /// </summary>
+    public TransformationBuilder() 
+    {
         this.transformations = new List<ITransformation>();
     }
 
-    public void buildTransformations(List<string> transformations) {
-        foreach(string t in transformations) {
+    /// <summary>
+    /// Converts a list of transformation string requests to ITransformation commands
+    /// </summary>
+    /// <param name="transformations"></param>
+    public void buildTransformations(List<string> transformations) 
+    {
+        foreach(string t in transformations) 
+        {
             ITransformation? transformation = TransformationFactory.getTransformation(t);
             if (transformation is not null)
                 this.transformations.Add(transformation);
         }
     }
 
-    public List<ITransformation> getTransformations() {
+    /// <summary>
+    /// Retrieves a list of ITransformation commands
+    /// </summary>
+    /// <returns></returns>
+    public List<ITransformation> getTransformations() 
+    {
         return new List<ITransformation>(this.transformations);
     }
 }
