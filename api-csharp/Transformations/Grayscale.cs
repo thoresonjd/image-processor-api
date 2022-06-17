8022d71764a767db811953da68767bd7e7278426
Justin Thoreson <thoresonjd@gmail.com>
2022-06-16 21:11:15 -0700

    Grayscale implemented

--- api-csharp/Transformations/Grayscale.cs
+++ api-csharp/Transformations/Grayscale.cs
@@ -1,3 +1,4 @@
+using SixLabors.ImageSharp.Processing;
 using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
@@ -7,6 +8,6 @@ class Grayscale : ITransformation {
     public Grayscale() {}
 
     public void  transform(ref SharpImage image) {
-        
+        image.Mutate(x => x.Grayscale());
     }
 }
--- api-csharp/Transformations/GrayscalePercentage.cs
+++ api-csharp/Transformations/GrayscalePercentage.cs
@@ -1,12 +1,17 @@
+using SixLabors.ImageSharp.Processing;
 using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
 class GrayscalePercentage : ITransformation {
 
-    public GrayscalePercentage() {}
+    private float percentage;
+
+    public GrayscalePercentage(float percentage) {
+        this.percentage = percentage;
+    }
 
     public void transform(ref SharpImage image) {
-      
+        image.Mutate(x => x.Grayscale(percentage));
     }
 }
