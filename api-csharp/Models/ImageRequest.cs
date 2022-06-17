b78a59b88b4c86efc57b191935d91af6f4cfe733
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:43:55 -0700

    Documentation for ImageRequest

--- api-csharp/Models/ImageRequest.cs
+++ api-csharp/Models/ImageRequest.cs
@@ -1,7 +1,12 @@
 namespace ImageProcessor.Models;
 
-public class ImageRequest {
-    
+/// <summary>
+/// ImageRequest
+/// A model representing the JSON structure of a received POST request
+/// The image and the list of transformations are contained in a request
+/// </summary>
+public class ImageRequest 
+{
     public string image { get; set; } = string.Empty;
 
     public List<string> transformations { get; set; } = new List<string>();
