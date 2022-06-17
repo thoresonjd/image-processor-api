db75d4261d40f8ff5ddd5ac1563af88698af1a44
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:43:40 -0700

    Documentation for ImageVerifier

--- api-csharp/ImageVerifier.cs
+++ api-csharp/ImageVerifier.cs
@@ -2,16 +2,31 @@ using Newtonsoft.Json;
 
 namespace ImageProcessor;
 
-class ImageVerifier {
-
+/// <summary>
+/// ImageVerifier<br />
+/// Handles image verification
+/// </summary>
+class ImageVerifier 
+{
     private Dictionary<string, string>? supportedTypes;
 
-    public ImageVerifier() {
+    /// <summary>
+    /// Constructor if the ImageVerifier class<br />
+    /// Initializes the dictionary of supported MIME types
+    /// </summary>
+    public ImageVerifier() 
+    {
         string json = File.ReadAllText("MIMETypes.json");
         supportedTypes = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
     }
 
-    public bool isSupportedImage(string image) {
+    /// <summary>
+    /// Checks if an image has a supported MIME type
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns>True if image is of a supported type, false otherwise</returns>
+    public bool isSupportedImage(string image) 
+    {
         foreach (string mimeType in supportedTypes!.Keys)
             if (image.StartsWith(mimeType))
                 return true;
