d52debf6ff60c6e7343867cbbccec66d610159b1
Justin Thoreson <thoresonjd@gmail.com>
2022-06-17 11:44:43 -0700

    Documentation for Resize command

--- api-csharp/Transformations/Resize.cs
+++ api-csharp/Transformations/Resize.cs
@@ -3,18 +3,35 @@ using SharpImage = SixLabors.ImageSharp.Image;
 
 namespace ImageProcessor.Transformations;
 
-class Resize : ITransformation {
-
+/// <summary>
+/// Resize<br />
+/// Implements the ITransormation interface<br />
+/// Allows for an image to be resized
+/// </summary>
+class Resize : ITransformation 
+{
     private uint width;
 
     private uint height;
 
-    public Resize(uint width, uint height) {
+    /// <summary>
+    /// Constructor of the Resize command<br />
+    /// Sets the width and height for the resize command
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    public Resize(uint width, uint height) 
+    {
         this.width = width;
         this.height = height;
     }
 
-    public void transform(ref SharpImage image) {
+    /// <summary>
+    /// Resizes an image
+    /// </summary>
+    /// <param name="image"></param>
+    public void transform(ref SharpImage image) 
+    {
         image.Mutate(x => x.Resize((int)width, (int)height));
     }
 }
